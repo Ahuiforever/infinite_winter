adapted the screen coordinates for a 1k resolution with 100% scaling;some bug fixes

infinite_winter_advanced.py
@@ -1,10 +1,14 @@
 # import requirements
 # local
 import time
+from datetime import datetime
+import gc
 
 # pywin23
 import win32gui
 import win32api
+
+# pyautogui
 import pyautogui
 # pywinauto
 from pywinauto import Application
@@ -12,10 +16,34 @@ from pywinauto import Application
 # numpy
 import numpy as np
 
-# opencv-python
+# opencv
 import cv2
 
+# psutil
+import psutil
+
+
 BIAS = (100, 100)
+
+
+def print_memory_info():
+    # 获取虚拟内存信息
+    memory_info = psutil.virtual_memory()
+
+    # 获取交换空间信息
+    swap_info = psutil.swap_memory()
+
+    print("Memory Information:")
+    print(f"Total memory: {memory_info.total / (1024 ** 3):.2f} GB")
+    print(f"Available memory: {memory_info.available / (1024 ** 3):.2f} GB")
+    print(f"Used memory: {memory_info.used / (1024 ** 3):.2f} GB")
+    print(f"Memory usage: {memory_info.percent}%")
+
+    print("\nSwap Information:")
+    print(f"Total swap space: {swap_info.total / (1024 ** 3):.2f} GB")
+    print(f"Used swap space: {swap_info.used / (1024 ** 3):.2f} GB")
+    print(f"Free swap space: {swap_info.free / (1024 ** 3):.2f} GB")
+    print(f"Swap usage: {swap_info.percent}%")
 
 
 class Coordinates:
@@ -51,45 +79,57 @@ class Matcher:
 
     # @staticmethod
     def color(self, xy: tuple, target_color: tuple) -> bool:
+        # try:
+        #     print_memory_info()
         screen_shot = self.win.capture_as_image()
-        # screen_shot = pyautogui.screenshot()
+        # except Exception as e:
+        #     print(e)
+        #     print(datetime.now())
+        # screen_shot = pyautogui.screenshot(region=win32gui.GetWindowRect(self.hwnd))
         image = np.array(screen_shot)
+        # cv2.imshow('image', image)
+        # cv2.waitKey(0)
         pixel_color = image[xy[1], xy[0]]  # height=x, width=y
         if tuple(pixel_color) == target_color:
             return True
         return False
 
-    def image(self, template_path, threshold=0.8, scale_factor=0.9):
+    def image(self, template_path, threshold=0.9, scale_factor=0.9):
         if template_path is None:
-            template_path = 'donate_tip.png'
+            template_path = 'donate_tip_4kx150.png'
         template = cv2.imread(template_path)
         target = self.win.capture_as_image()
         target = np.array(target)
         target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
 
         w, h = template.shape[:2][::-1]
+        wh = (w, h)
         scale = 1.0
         matches = []
 
-        # while w > 30 and h > 30:  # 如果模板图像尺寸过小，则停止缩放
-        #     # 在目标图像中进行模板匹配
-        #     result = cv2.matchTemplate(target, template, cv2.TM_CCOEFF_NORMED)
-        #
-        #     # 找到匹配度大于 threshold 的位置
-        #     loc = np.where(result >= threshold)
-        #
-        #     # 记录匹配结果
-        #     if len(loc[0]) > 0:
-        #         matches.extend(list(zip(*loc[::-1])))
-        #
-        #     # 缩小模板图像
-        #     scale *= scale_factor
-        #     w = int(w * scale)
-        #     h = int(h * scale)
-        #     template = cv2.resize(template, (w, h))
+        while w > 15 and h > 15:  # 如果模板图像尺寸过小，则停止缩放
+            # 在目标图像中进行模板匹配
+            result = cv2.matchTemplate(target, template, cv2.TM_CCOEFF_NORMED)
+
+            # 找到匹配度大于 threshold 的位置
+            loc = np.where(result >= threshold)
+
+            # 记录匹配结果
+            if len(loc[0]) > 0:
+                matches.extend(list(zip(*loc[::-1])))
+
+            # 缩小模板图像
+            scale *= scale_factor
+            w = int(w * scale)
+            h = int(h * scale)
+            template = cv2.resize(template, (w, h))
 
         if len(matches) > 0:
-            return matches
+            # for pt in zip(*loc[::-1]):  # loc[::-1] 转换成 (x, y) 格式
+            #     cv2.rectangle(target, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 2)
+            # cv2.imshow('Matches', target)
+            # cv2.waitKey(0)
+            return matches[0][0] + wh[0], matches[0][1] + wh[1]
         else:
             result = cv2.matchTemplate(target, template, cv2.TM_CCOEFF_NORMED)
             loc = np.where(result >= threshold)
@@ -100,7 +140,8 @@ class Matcher:
 
             if len(loc[0]) > 0:
                 # return [(pt[0] + w // 2, pt[1] + h // 2) for pt in zip(*loc[::-1])]
-                return list(zip(*loc[::-1]))
+                matches.extend(list(zip(*loc[::-1])))
+                return matches[0][0] + wh[0], matches[0][1] + wh[1]
             else:
                 return
 
@@ -222,11 +263,11 @@ class AutoTab:
                         pyautogui.click(self.coord['close_surpass_capacity'][0],
                                         self.coord['close_surpass_capacity'][0])
                         time.sleep(1)
-                    elif self.match.color(self.coord['accelerate_check'][0],
-                                          self.coord['accelerate_check'][1]):
-                        pyautogui.click(self.coord['close_accelerate'][0],
-                                        self.coord['close_accelerate'][0])
-                        time.sleep(1)
+                    # elif self.match.color(self.coord['accelerate_check'][0],
+                    #                       self.coord['accelerate_check'][1]):
+                    #     pyautogui.click(self.coord['close_accelerate'][0],
+                    #                     self.coord['close_accelerate'][0])
+                    #     time.sleep(1)
                     pyautogui.click(self.coord['back'][0], self.coord['back'][1])
             else:
                 pyautogui.click(self.coord['back'][0], self.coord['back'][1])
@@ -243,7 +284,7 @@ class AutoTab:
             time.sleep(1)
             donate_tip = self.match.image(None, threshold=0.9)
             if donate_tip:  # if there exits donate recommendation
-                donate_tip = self.coord.to_absolute((donate_tip[0][0], donate_tip[0][1]))
+                donate_tip = self.coord.to_absolute((donate_tip[0], donate_tip[1]))
                 pyautogui.click(donate_tip[0], donate_tip[1])
                 time.sleep(1)
                 # pyautogui.position(self.coord['donating'][0], self.coord['donating'][0])
@@ -283,6 +324,10 @@ class AutoTab:
     def filter_for_snow_monster(self):
         ...
 
+    @staticmethod
+    def prevent_screen_from_sleeping():
+        pyautogui.press('alt')
+
     def __call__(self):
         self.world_check()
         self.triangle_check()
@@ -291,7 +336,7 @@ class AutoTab:
         self.warehouse()  # donate
         self.assemble()
         self.explore()
-        # self.back_check()
+        self.prevent_screen_from_sleeping()
         time.sleep(10)
 
 
@@ -320,7 +365,7 @@ def main(coord: Coordinates):
 if __name__ == '__main__':
     # initialize coordinate
     # 4k, 150%
-    c = Coordinates({
+    c4kx150 = Coordinates({
         # point_name: (x, y)
         # point_check: [(x, y), (R, G, B)]
         'tri': (125, 855),
@@ -377,4 +422,64 @@ if __name__ == '__main__':
         'accelerate_check': [(985, 725), (197, 220, 255)],
         'snow_monster_check': [(235, 620), (126, 49, 21)]
     })
-    main(c)
+    # 1k, 100%
+    c1kx100 = Coordinates({
+        # point_name: (x, y)
+        # point_check: [(x, y), (R, G, B)]
+        'tri': (115, 485),
+        'tri_check': [(398, 487), (255, 255, 255)],
+        'town': (150, 310),
+        'town_check': [(150, 310), (95, 171, 230)],
+        'town_logo': (510, 900),
+        'town_logo_check': [(510, 880), (255, 253, 185)],
+        'dun': (368, 482),
+        'dun_check': [(368, 482), (255, 30, 31)],
+        'mao': (368, 528),
+        'mao_check': [(368, 528), (255, 30, 31)],
+        'she': (368, 573),
+        'she_check': [(368, 573), (255, 30, 31)],
+        'point': (335, 500),
+        'train': (435, 650),
+        # 'train_check': [(1195, 1505), (96, 143, 216)],
+        'long_train': (440, 900),
+        # 'long_train_check': [(1413, 2033), (79, 165, 252)],
+        # 'training_check': [(968, 1667), (39, 89, 141)],
+        'back': (130, 170),
+        'back_check': [(130, 170), (203, 255, 255)],
+        'help': (440, 835),
+        'help_check': [(440, 835), (22, 169, 50)],
+        'drag_point': (200, 500),
+        'warehouse': (358, 650),
+        'warehouse_check': [(358, 650), (31, 255, 108)],
+        'blank': (335, 770),
+        'assemble': (525, 480),
+        'assemble_check': [(525, 480), (255, 255, 255)],
+        'join': (500, 380),
+        'join_check': [(500, 380), (37, 183, 86)],
+        'march_out': (450, 900),
+        'march_out_check': [(485, 900), (79, 165, 252)],
+        'close_need_more_queue': (508, 355),
+        'need_more_queue_check': [(508, 355), (197, 220, 255)],
+        'close_surpass_capacity': (448, 401),
+        'surpass_capacity_check': [(448, 401), (187, 209, 242)],
+        'average': (230, 880),
+        'donate1': (368, 462),
+        'donate2': (368, 392),
+        'donate1_check': [(368, 462), (255, 30, 31)],  # lower donate
+        'donate2_check': [(368, 392), (255, 30, 31)],
+        'recruit_check': [(132, 476), (113, 79, 62)],  # in case of false detect of hero_recruit
+        'alliance': (440, 900),
+        'tech': (440, 730),
+        'donating': (420, 785),
+        'close_donation': (506, 266),
+        'explore': (150, 900),
+        'explore_check': [(169, 885), (255, 30, 31)],
+        'collect': (490, 720),
+        'long_collect': (330, 720),
+
+        # 没改
+        'close_accelerate': (985, 725),
+        'accelerate_check': [(985, 725), (197, 220, 255)],
+        'snow_monster_check': [(235, 620), (126, 49, 21)]
+    })
+    main(c1kx100)